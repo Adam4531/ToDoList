fdf8f005303c0d46398f19d06b96d644d62e5b00
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2022-05-22 16:41:56 +0200

    Method elementIsDone added to ToDoList class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+//        System.out.println("1. \u2705");
         display();
     }
 
--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -9,32 +9,39 @@ public class ToDoList {
     private String title;
     private List<String> thingsToDo;
     private final LocalDateTime createAt;
-    private List<Boolean> isDone;
+    private List<Boolean> isDoneList;
     private LocalDateTime doneAt;
 
 
     public ToDoList(String aTitle) {
         title = aTitle;
         thingsToDo = new ArrayList<>();
-        isDone = new ArrayList<>();
+        isDoneList = new ArrayList<>();
         createAt = LocalDateTime.now();
     }
 
 
     public void addElement(String element){
         thingsToDo.add(element);
-        isDone.add(false);
+        isDoneList.add(false);
     }
 
 
     public boolean isListDone(){
-        for (Boolean isDone: isDone) {
+        for (Boolean isDone: isDoneList) {
             if(isDone.equals(false)){
                 return false;
             }
         }
         this.doneAt = LocalDateTime.now();
         return true;
+    }
+
+    public void elementIsDone(int index){
+        String element = thingsToDo.get(index);
+        thingsToDo.add(index, element + " \u2705");
+        isDoneList.set(index, true);
+//      System.out.println();
     }
 
 
