e5b1833fe5fab88d9c571f1abe2de12b1610a03b
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2022-05-24 20:54:38 +0200

    menu and display methods added to Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("1. \u2705");
+        Scanner input = new Scanner(System.in);
         display();
+        int option = input.nextInt();
+        menu(option);
     }
 
-    static void display(){ //TODO Create display menu and make a tests
-        System.out.println();
+    static void display() { //TODO Create display menu and make a tests
+        System.out.println("=================");
+        System.out.println("1. Create new list");
+        System.out.println("2. Choose existing list");
+        System.out.println("3. Exit");
+        System.out.println("=================");
+    }
+
+    static void menu(int option) {
+        switch (option){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            default:
+                throw new IllegalArgumentException("");
+        }
+
     }
 }
