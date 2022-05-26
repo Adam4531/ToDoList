c90eaed8b42bc65bbb259f02bd7dd8f9979bf9ef
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2022-05-26 22:23:49 +0200

    ToDoListUtils added to project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,34 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        display();
-        int option = input.nextInt();
-        menu(option);
+        int choice = display();
+
     }
 
-    static void display() { //TODO Create display menu and make a tests
+    static int display() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Main Menu:");
         System.out.println("=================");
         System.out.println("1. Create new list");
         System.out.println("2. Choose existing list");
         System.out.println("3. Exit");
         System.out.println("=================");
+        System.out.print("> ");
+        return input.nextInt();
     }
 
     static void menu(int option) {
-        switch (option){
-            case 1:
+        while (option != -1) {
+            switch (option) {
+                case 1:
+                    ToDoListUtils.create();
+                    break;
+                case 2:
 
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            default:
-                throw new IllegalArgumentException("");
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Enter a valid option (from 1 to 3)");
+            }
         }
-
     }
+
 }
---
+++ src/main/java/ToDoListUtils.java
@@ -0,0 +1,6 @@
+public abstract class ToDoListUtils {
+
+    public static ToDoList create(String name){
+        return new ToDoList(name);
+    }
+}
