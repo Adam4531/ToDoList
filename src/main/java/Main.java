8bcc61ea0f582aa33a6589242398d0862b1e2522
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2022-06-21 22:21:32 +0200

    Create a ToDoList method added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,48 +2,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int choice = display();
+        int choice = displayMainMenu();
 
-        while (choice != 3) {
+        while (choice != 0) {
             switch (choice) {
                 case 1:
-                    ToDoListFactory.create();
+                    create();
                     break;
                 case 2:
 
                     break;
                 default:
-                    System.out.println("Enter a valid option (from 1 to 3)");
+                    System.out.println("Enter a valid option (from 0 to 2)");
             }
         }
 
     }
 
-    static int display() {
+    static int displayMainMenu() {
         Scanner input = new Scanner(System.in);
         System.out.println("Main Menu:");
         System.out.println("=================");
+        System.out.println("0. Exit");
         System.out.println("1. Create new list");
         System.out.println("2. Choose existing list");
-        System.out.println("3. Exit");
         System.out.println("=================");
         System.out.print("> ");
         return input.nextInt();
     }
 
-//    static void menu(int option) {
-//        while (option != 3) {
-//            switch (option) {
-//                case 1:
-//                    ToDoListFactory.create();
-//                    break;
-//                case 2:
-//
-//                    break;
-//                default:
-//                    System.out.println("Enter a valid option (from 1 to 3)");
-//            }
-//        }
+    public static ToDoList create(){
+        return new ToDoList(setTitle());
     }
 
-}
+    public static String setTitle(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    }
+
+
